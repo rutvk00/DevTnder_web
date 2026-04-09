c7791ccae6470ee115b6a310b13cb10901123126
rutvk00 <prajapatirutvik107@gmail.com>
2026-04-09 21:08:15 +0530

    init-database and basic signup api

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
-express = require("express")
-
+const express = require("express")
+const req = require("express/lib/request")
+const connectDB = require("./config/database")
 app = express()
 
-app.use("/" , (req, res) => {
-    res.send("Hello World from server")
+const User = require("./models/user")
+
+app.post("/signup" , async (req , res) => {
+
+    const user = new User({
+        firstName : "Rutvik",
+        lastName : "Prajapati",
+        emailId : "rutvik@mail.com",
+        password : "12345"
+    })
+
+    try{
+        await user.save()
+        res.send("User Added Sucessfully !!")
+    }catch(error){
+        res.status(400).send("Error saving user : " + error.message)
+    }
 })
 
+connectDB()
+    .then(() => {
+        console.log("Database connection established");
+        app.listen(7777 , () => {
+            console.log("server is listening on port 7777...")
+        });
+    })
+    .catch(() => {
+        console.error("Database cannot be connected !!")
+    })
 
-app.listen(7777 , () => {
-    console.log("server is listening on port 7777...")
-})
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://rutvik00:Password@cluster0.728udnf.mongodb.net/Dev_Tinder");
+};
+
+module.exports = connectDB;
